5f115392dab0d17333a0808c6e62fa31b7666682
Zsolt Nagy <zsoltnagy@Zsolts-MacBook-Air.local>
2018-01-24 15:34:17 +0100

    added static files for serving

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const config = require('config');
 
 const app = express();
 
+app.use(express.static('build'));
+
 app.listen(config.port, () => {
-  console.log(config)
   console.log(`The server is running on port ${config.port}`);
 });
+
