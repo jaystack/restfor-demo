f6a6c572c4cd7b8706f585e7ef3ff9e56d6c94aa
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-01-26 19:16:02 +0100

    wip

--- src/views/index.js
+++ src/views/index.js
@@ -2,15 +2,30 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import Person from 'material-ui-icons/Person';
 import MyCustomIsExpired from './MyCustomIsExpired';
+import types from 'restfor/types';
 
 export default register => {
-  // selections
-  /* register.grid.action('task', 'My Delete', { sendEmail: 'boolean', userid: MyCustomReactComp }, async ({ params, invoke, state, selections }) => {
-    await invoke('GET', 'task', '/actions/count', { body: selections }, (state, error, result) => {
-      alert(result.count);
-      return state;
-    });
-  }); */
+  register.grid.action(
+    'user',
+    'Add task',
+    async ({ params, invoke, state, selection }) => {
+      await invoke('POST', 'task', '/actions/addTaskForUsers', { body: selection });
+    },
+    {
+      condition: ({ selection }) => selection.length > 0,
+      params: {
+        sendEmail: types.bool(),
+        title: types.string(),
+        deadline: types.date(),
+        taskType: types.enum('primary', 'secondary'),
+        noSense: ({ value, onChange }) => <TextField value={value} onChange={evt => onChange(evt.target.value)} />
+      }
+    }
+  );
+
+  register.grid.action('task', 'Action without params', ({ selection }) => {
+    setTimeout(() => alert(`Selection: ${selection.join(', ')}`), 500);
+  });
 
   register.grid.property('task', 'isExpired', MyCustomIsExpired);
 
