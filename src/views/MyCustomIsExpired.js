1e1eb6073b1ef9081ca631d259ab2df943d4f5f8
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-01-26 15:09:10 +0100

    wip

--- src/views/MyCustomIsExpired.js
+++ src/views/MyCustomIsExpired.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Chip from 'material-ui/Chip';
 
-export default ({ record }) =>
-  record.deadline && Date.parse(record.deadline) < Date.now() ? (
+export default ({ record, invoke }) => {
+  return record.deadline && Date.parse(record.deadline) < Date.now() ? (
     <Chip label="Expired" style={{ backgroundColor: '#F44336', color: 'white' }} />
   ) : null;
+};
--- src/views/index.js
+++ src/views/index.js
@@ -4,16 +4,29 @@ import Person from 'material-ui-icons/Person';
 import MyCustomIsExpired from './MyCustomIsExpired';
 
 export default register => {
-  //register.grid.property('task', 'deadline', ({ value }) => new Date(value).toLocaleTimeString());
+  // selections
+  /* register.grid.action('task', 'My Delete', { sendEmail: 'boolean', userid: MyCustomReactComp }, async ({ params, invoke, state, selections }) => {
+    await invoke('GET', 'task', '/actions/count', { body: selections }, (state, error, result) => {
+      alert(result.count);
+      return state;
+    });
+  }); */
 
   register.grid.property('task', 'isExpired', MyCustomIsExpired);
 
   register.details.property('task', 'isExpired', MyCustomIsExpired);
 
-  register.editor.property('task', 'UserId', ({ propertyName, value, onChange }) => (
-    <div>
-      <Person />{' '}
-      <TextField name={propertyName} type="number" value={value} onChange={evt => onChange(Number(evt.target.value))} />
-    </div>
-  ));
+  register.editor.property('task', 'UserId', ({ propertyName, value, onChange, invoke }) => {
+    return (
+      <div>
+        <Person />{' '}
+        <TextField
+          name={propertyName}
+          type="number"
+          value={value}
+          onChange={evt => onChange(Number(evt.target.value))}
+        />
+      </div>
+    );
+  });
 };
