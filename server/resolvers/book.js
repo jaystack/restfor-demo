ec8e95a8b53e67481d86e5f3e85073513fe9cbf2
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-02-02 15:44:25 +0100

    split schemas and resolvers files

---
+++ server/resolvers/book.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BookQuery: {
+    count: () => 42
+  }
+};
--- server/resolvers/hero.js
+++ server/resolvers/hero.js
@@ -1,9 +1,6 @@
 const heros = [{ id: 1, name: 'R2-D2' }, { id: 2, name: 'Luke' }, { id: 3, name: 'Darth Vader' }];
 
 module.exports = {
-  BookQuery: {
-    count: () => 42
-  },
   HeroQuery: {
     all: () => ({ items: heros, count: heros.length }),
     item: (_, { id }) => heros.find(hero => hero.id === id)
