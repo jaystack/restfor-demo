8c090a5e9135e6e22f2a0a3d1526d070c8b84fe4
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-02-02 13:54:28 +0100

    default order in tasks

--- server/models/task.js
+++ server/models/task.js
@@ -1,24 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
-  const Task = sequelize.define('Task', {
-    title: DataTypes.STRING,
-    checked: { type: DataTypes.BOOLEAN, defaultValue: false, allowNull: false },
-    taskType: DataTypes.ENUM('primary', 'secondary'),
-    deadline: DataTypes.DATE
-  }, {
-    customOptions: {
-      hidden: false,
-      grid: {
-        visibleFields: [
-          'id','title', 'checked', 'taskType', 'deadline', 'UserId','isExpired'
-        ]
-      }
+  const Task = sequelize.define(
+    'Task',
+    {
+      title: DataTypes.STRING,
+      checked: { type: DataTypes.BOOLEAN, defaultValue: false, allowNull: false },
+      taskType: DataTypes.ENUM('primary', 'secondary'),
+      deadline: DataTypes.DATE
     },
-    segments: [
-      { segmentKey:'primary', label: 'primary tasks', segmentField: 'taskType',  segmentValue: 'primary'},
-      { segmentKey:'secondary', label: 'secondary tasks',  segmentField: 'taskType',  segmentValue: 'secondary' },
-      { segmentKey:'smallids', label: 'tasks with ids below 5',  where: { id:{ lt: 5}} },
-    ]
-  });
+    {
+      customOptions: {
+        hidden: false,
+        defaultOrder: [['title', 'ASC']],
+        grid: {
+          visibleFields: ['id', 'title', 'checked', 'taskType', 'deadline', 'UserId', 'isExpired']
+        }
+      },
+      segments: [
+        { segmentKey: 'primary', label: 'primary tasks', segmentField: 'taskType', segmentValue: 'primary' },
+        { segmentKey: 'secondary', label: 'secondary tasks', segmentField: 'taskType', segmentValue: 'secondary' },
+        { segmentKey: 'smallids', label: 'tasks with ids below 5', where: { id: { lt: 5 } } }
+      ]
+    }
+  );
 
   Task.associate = ({ Task, User }) =>
     Task.belongsTo(User, {
